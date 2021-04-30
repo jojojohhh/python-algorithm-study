cf89078806cdbf1321b5936e23eba2c24523fd34
jojojohhh <qwhtkdwp@gmail.com>
2021-04-30 18:15:03 +0900

    2021/04/30

---
+++ programmers/kakao_bad_user.py
@@ -0,0 +1,24 @@
+from itertools import permutations
+
+
+def compare_to(user, ban):
+    for i in range(len(user)):
+        if len(user[i]) != len(ban[i]):
+            return False
+        for j in range(len(ban[i])):
+            if ban[i][j] != "*" and user[i][j] != ban[i][j]:
+                return False
+    return True
+
+
+def solution(user_id, banned_id):
+    answer = list()
+    for i in permutations(user_id, len(banned_id)):
+        if compare_to(i, banned_id):
+            i = set(i)
+            if i not in answer:
+                answer.append(i)
+    return len(answer)
+u = ["frodo", "fradi", "crodo", "abc123", "frodoc"]
+b = ["fr*d*", "*rodo", "******", "******"]
+print(solution(u, b))
