a7099cdc1597745ac2a5aeec44a2329b209d781c
jojojohhh <qwhtkdwp@gmail.com>
2021-05-07 14:32:38 +0900

    2021/05/07

--- programmers/programmers_budget.py
+++ programmers/programmers_budget.py
@@ -1,10 +1,19 @@
-def solution(d, budget):
-    d.sort()
-    answer = 0
-    for n in d:
-        if n <= budget:
-            answer += 1
-            budget -= n
-        else:
-            break
+from collections import deque
+from math import inf
+
+
+def solution(N, road, K):
+    dic = {}
+    for n in road:
+        dic[n[0]] = dic.get(n[0], []) + [[n[1], n[2]]]
+        dic[n[1]] = dic.get(n[1], []) + [[n[0], n[2]]]
+    dist = {i: inf if i != 1 else 0 for i in range(1, N + 1)}
+    que = deque([1])
+    while que:
+        cur = que.popleft()
+        for nxt in dic[cur]:
+            if dist[nxt[0]] > dist[cur] + nxt[1]:
+                dist[nxt[0]] = dist[cur] + nxt[1]
+                que.append(nxt[0])
+    answer = len([1 for _, a in dist.items() if a <= K])
     return answer
