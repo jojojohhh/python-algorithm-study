b6cde9f8b8d788c8a356622ba31e2b36d49c3688
jojojohhh <qwhtkdwp@gmail.com>
2021-05-17 08:45:41 +0900

    2021/05/17

---
+++ programmers/programmers_expectation_match.py
@@ -0,0 +1,9 @@
+def solution(n,a,b):
+    answer = 1
+    while True:
+        if abs(a - b) == 1 and abs(a // 2 - b // 2) == 1:
+            break
+        a = a // 2 if a % 2 == 0 else a // 2 + 1 
+        b = b // 2 if b % 2 == 0 else b // 2 + 1
+        answer += 1
+    return answer
