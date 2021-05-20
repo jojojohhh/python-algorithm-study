abe9c772e9202d37b55605678136f768cc4aed42
jojojohhh <qwhtkdwp@gmail.com>
2021-05-20 20:33:38 +0900

    2021/05/20

--- programmers/kakao_convert_bracket.py
+++ programmers/kakao_convert_bracket.py
@@ -1,45 +1,30 @@
+from collections import deque
+
+
 def check(p):
     st = []
     for i in p:
         if i == '(':
             st.append(i)
-        else:
-            if len(st) == 0:
-                return False
+        elif st:
             st.pop()
-    if len(st) == 0:
-        return True
-
-
-def recursion(p):
-    if check(p):
-        return p
-    u = [p[0]]
-    for i in range(1, len(p)):
-        u.append(p[i])
-        if u.count('(') == u.count(')'):
-            v = list(p[i + 1:])
-            break
-    if not check(u):
-        for i in range(1, len(u) - 1):
-            if u[i] == '(':
-                u[i] = ')'
-            else:
-                u[i] = '('
-        new_u = ['(']
-        new_u.extend(u[1:len(u) - 1])
-        new_u.append(')')
-        new_u.extend(recursion(v))
-        return new_u
-    else:
-        u.extend(recursion(v))
-        return u
+    return not st
 
 
 def solution(p):
     if check(p) or len(p) == 0:
         return p
-    return ''.join(recursion(list(p)))
+    p_que = deque(p)
+    u = ""
+    while p_que:
+        u += p_que.popleft()
+        if u.count('(') == u.count(')'):
+            break
+    v = ''.join(list(p_que))
+    if check(u):
+        return u + solution(v)
+    else:
+        return '(' + solution(v) + ')' + ''.join(['(' if u[i] == ')' else ')' for i in range(1, len(u) - 1)])
 
 
 print(solution("()))((()"))
