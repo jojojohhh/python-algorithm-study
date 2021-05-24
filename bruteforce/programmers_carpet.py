2b8bb0f8a3ebf39125643174f8e41be8ae4db4f5
jojojohhh <qwhtkdwp@gmail.com>
2021-05-24 23:56:43 +0900

    2021/05/24

--- bruteforce/programmers_carpet.py
+++ bruteforce/programmers_carpet.py
@@ -1,7 +1,7 @@
 import math
 
+
 def solution(brown, yellow):
-    answer = []
     for i in range(1, int(math.sqrt(yellow)) + 1):
         if yellow % i == 0:
             if 2 * (i + (yellow / i)) + 4 == brown:
