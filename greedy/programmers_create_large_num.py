287ac145094fe7385969957bf0d5f379f033a2b7
jojojohhh <qwhtkdwp@gmail.com>
2021-05-25 18:14:52 +0900

    2021/05/25

---
+++ greedy/programmers_create_large_num.py
@@ -0,0 +1,10 @@
+def solution(number, k):
+    st = [number[0]]
+    for num in number[1:]:
+        while st and st[-1] < num and k > 0:
+            k -= 1
+            st.pop()
+        st.append(num)
+    if k != 0:
+        st = st[:-k]
+    return ''.join(st[:-k]) if k != 0 else ''.join(st)
