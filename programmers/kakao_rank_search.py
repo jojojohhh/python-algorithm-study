31c739dfc86aa5fa1ff527d846470fb89ef8a368
jojojohhh <qwhtkdwp@gmail.com>
2021-05-27 22:23:26 +0900

    2021/05/27

--- programmers/kakao_rank_search.py
+++ programmers/kakao_rank_search.py
@@ -1,5 +1,11 @@
 def solution(info, query):
-    rel = [i.split(' ') for i in info]
-    query = [list(filter(lambda x: x != "and", q.split(' '))) for q in query]
-    answer = [len(list(filter(lambda x: ((x[0] == q[0] or q[0] == '-') and (x[1] == q[1] or q[1] == '-') and (x[2] == q[2] or q[2] == '-') and (x[3] == q[3] or q[3] == '-') and (int(x[4]) >= int(q[4]))), rel))) for q in query]
+    answer = []
     return answer
+
+
+print(solution(
+    ["java backend junior pizza 150", "python frontend senior chicken 210", "python frontend senior chicken 150",
+     "cpp backend senior pizza 260", "java backend junior chicken 80", "python backend senior chicken 50"],
+    ["java and backend and junior and pizza 100", "python and frontend and senior and chicken 200",
+     "cpp and - and senior and pizza 250", "- and backend and senior and - 150", "- and - and - and chicken 100",
+     "- and - and - and - 150"]))
