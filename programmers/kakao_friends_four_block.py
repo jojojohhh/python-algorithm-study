7efb86becaa88c158777ce17cc96213912e2bc5c
jojojohhh <qwhtkdwp@gmail.com>
2021-06-09 15:41:41 +0900

    2021/06/09

--- programmers/kakao_friends_four_block.py
+++ programmers/kakao_friends_four_block.py
@@ -1,43 +1,17 @@
-from collections import deque
-
-
-def check_block(m, n, x, y, board, cur):
-    que = deque([[y, x]])
-    new_board = [list(b) for b in board]
-    cardies = [[0, 1], [1, 0], [1, 1]]
-    coord = {(y, x)}
-    cnt = 0
-    while que:
-        qy, qx = que.popleft()
-        if qy == m - 1 or qx == n - 1:
-            continue
-        new_que = deque()
-        for dy, dx in cardies:
-            nxt_x, nxt_y = dx + qx, dy + qy
-            if board[nxt_y][nxt_x] != cur:
-                break
-            new_que.append([nxt_y, nxt_x])
-        if len(new_que) == len(cardies):
-            new_board[y][x] = '0'
-            cnt += 1
-            for dy, dx in new_que:
-                if new_board[dy][dx] != '0':
-                    new_board[dy][dx] = '0'
-                    coord.add((dy, dx))
-                    cnt += 1
-            que.extend(new_que)
-    print(coord)
-    return new_board
-
-
 def solution(m, n, board):
     answer = 0
-    for i in range(m):
-        for j in range(n):
-            if board[i][j] != '0':
-                board = check_block(m, n, j, i, board, board[i][j])
-    print(board)
-    return answer
-
-
-print(solution(4, 5, ["CCBDE", "AAADE", "AAABF", "CCBBF"]))
+    board = list(map(list, zip(*board)))
+    while True:
+        coord = set()
+        for i in range(1, n):
+            for j in range(1, m):
+                if board[i][j] == board[i - 1][j] == board[i][j - 1] == board[i - 1][j - 1] != '-1':
+                    coord |= {(i, j), (i - 1, j), (i, j - 1), (i - 1, j - 1)}
+        for i, j in coord:
+            board[i][j] = '-1'
+        for i in range(len(board)):
+            mov_block = ['-1'] * board[i].count('-1')
+            board[i] = mov_block + [b for b in board[i] if b != '-1']
+        if len(coord) == 0:
+            return answer
+        answer += len(coord)
