270faa73c3eac408df960e72925b225d756d90dd
jojojohhh <qwhtkdwp@gmail.com>
2021-06-11 22:57:56 +0900

    2021/06/11

--- programmers/programmers_triangular_snail.py
+++ programmers/programmers_triangular_snail.py
@@ -1,6 +1,34 @@
+from itertools import chain
+
+
 def solution(n):
-    answer = []
-    return answer
+    s = [[0] * i for i in range(1, n + 1)]
+    i, j, cnt = 0, 0, 1
+    last = sum([len(i) for i in s])
+    nxt_coord = {'RIGHT': [0, 1], 'DOWN': [1, 0], 'UP': [-1, -1]}
+    state = 'DOWN'
+    while True:
+        if cnt > last:
+            break
+        if not s[i][j]:
+            s[i][j] = cnt
+            if i == n - 1 and j < len(s[i]) - 1:
+                state = 'RIGHT'
+            elif j == len(s[i]) - 1 and i != 0:
+                state = 'UP'
+            cnt += 1
+        else:
+            nxt_i, nxt_j = nxt_coord[state]
+            i -= nxt_i
+            j -= nxt_j
+            if state == 'RIGHT':
+                state = 'UP'
+            elif state == 'UP':
+                state = 'DOWN'
+            else:
+                state = 'RIGHT'
+        nxt_i, nxt_j = nxt_coord[state]
+        i += nxt_i
+        j += nxt_j
+    return list(chain.from_iterable(s))
 
-
-print(5)
